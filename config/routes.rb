b04070282850640b5e05da2fd5bc093ddacbc8fc
Nozomi Morimoto <nozomi@NozominoMacBook-Pro.local>
2016-12-27 19:11:15 +0900

    hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'top' => 'home#top'
+
+  # get 'home/top'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
