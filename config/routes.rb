994c11fca8693ef5580edc238bb5326dddd55a39
takaya <ninataka_japan@yahoo.co.jp>
2016-12-28 09:27:05 +0900

    create user db and login ccontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/login'
+
   get '/' => 'home#index'
   # get 'home/top'
   # The priority is based upon order of creation: first created -> highest priority.
